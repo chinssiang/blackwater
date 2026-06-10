20d6ec082b0059795ef466655aa64b14990c8363
Hsiang Chin <chinssiang@gmail.com>
2026-06-10 17:05:41 +0800

    refactor: update product header title to use translation function for improved localization; streamline ProductCard rendering for better readability

--- src/app/(frontend)/[locale]/products/all/_components/PageProductsAll.tsx
+++ src/app/(frontend)/[locale]/products/all/_components/PageProductsAll.tsx
@@ -17,18 +17,14 @@ export function PageProductsAll({ data }: Props) {
 	return (
 		<div className="p-x-max min-h-main py-10 lg:py-17.5">
 			<ProductPageHeader
-				title="Products"
+				title={t.allProducts}
 				counts={[{ count: products?.length, forms: t.productCount }]}
 			/>
 
 			{products && products.length > 0 ? (
 				<div className="mb-20 grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2 lg:grid-cols-3 lg:gap-y-16 2xl:grid-cols-4 2xl:gap-x-10">
 					{products.map((product, index) => (
-						<ProductCard
-							key={product._id}
-							product={product}
-							index={index}
-						/>
+						<ProductCard key={product._id} product={product} index={index} />
 					))}
 				</div>
 			) : (
